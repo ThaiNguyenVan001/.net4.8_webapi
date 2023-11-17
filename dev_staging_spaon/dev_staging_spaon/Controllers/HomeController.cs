57406cef820bdf3a16311965ef92a873f761ff79
ThaiNguyenVan001 <91704658+ThaiNguyenVan001@users.noreply.github.com>
2023-11-17 14:15:07 +0700

    update

--- dev_staging_spaon/dev_staging_spaon/Controllers/HomeController.cs
+++ dev_staging_spaon/dev_staging_spaon/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace dev_staging_spaon.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-            ***;
             return View();
         }
     }
